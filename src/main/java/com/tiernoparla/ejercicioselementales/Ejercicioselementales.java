7a0084ea29bb710a4f8617c1791850a792a6128d
IvanRdaw <ivanr.daw@gmail.com>
2022-09-27 20:16:16 +0200

    añado print 1 a 10

--- src/main/java/com/tiernoparla/ejercicioselementales/Ejercicioselementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/Ejercicioselementales.java
@@ -3,5 +3,10 @@ package com.tiernoparla.ejercicioselementales;
 public class Ejercicioselementales {
     public static void main(String[] args) {
         System.out.println("hola");
+        
+        // printear nums pares del 1 al 10
+        for (int i = 1; i < 11; i=i+1) {
+            System.out.println(i); 
+        } //for 
     } // main
 } // ejercicios elementales
