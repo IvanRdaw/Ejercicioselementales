e32221a7fbd465aa4f81ff62c552f843bd45fe41
IvanRdaw <ivanr.daw@gmail.com>
2022-09-27 19:02:28 +0200

    añado src main java

---
+++ src/main/java/com/tiernoparla/ejercicioselementales/Ejercicioselementales.java
@@ -0,0 +1,7 @@
+package com.tiernoparla.ejercicioselementales;
+
+public class Ejercicioselementales {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    } // main
+} // ejercicios elementales
