40adc2a67374b0e36691591e91f54d962832bbb5
IvanRdaw <ivanr.daw@gmail.com>
2022-09-28 18:28:51 +0200

    printear del 1 al 10 con for y while

--- src/main/java/com/tiernoparla/ejercicioselementales/Ejercicioselementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/Ejercicioselementales.java
@@ -8,13 +8,20 @@ public class Ejercicioselementales {
         for (int i = 1; i < 11; i=i+1) {
             System.out.println(i); 
         } //for 
-        
-        // printear nums pares del 1 al 10 detectando impares        
+        /*
+        // printear nums pares del 1 al 10 detectando impares con for        
         for (int i = 1; i <= 10; i++) {
             if (i%2 == 0) {
                 System.out.println(i);
             } // if
         } // for
-        
+        */
+        // printear nums pares del 1 al 10 detectando impares con while
+        int n = 1;
+        while (n<11) {
+            if(n%2==0) {
+                System.out.println(n);
+            } // if
+        } // while
     } // main
 } // ejercicios elementales
