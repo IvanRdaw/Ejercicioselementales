a967542d95003c67504ddd8838fa8d60cc30ff5b
alexandarsvetlinov <alexandarsvetlinov.ivanov@tiernoparla.es>
2022-09-28 17:51:27 +0200

    printear nums pares del 1 al 10 finalizado

--- src/main/java/com/tiernoparla/ejercicioselementales/Ejercicioselementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/Ejercicioselementales.java
@@ -8,5 +8,13 @@ public class Ejercicioselementales {
         for (int i = 1; i < 11; i=i+1) {
             System.out.println(i); 
         } //for 
+        
+        // printear nums pares del 1 al 10 detectando impares        
+        for (int i = 1; i <= 10; i++) {
+            if (i%2 == 0) {
+                System.out.println(i);
+            } // if
+        } // for
+        
     } // main
 } // ejercicios elementales
